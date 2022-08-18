dc5d91c047844502bf4fed95a2babaa8c081262f
Armazok <nikolai.borsin@mail.ru>
2022-08-19 02:29:01 +0500

    *

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import {Introduction} from "./ReactRouterDom/01_Introduction/01_introduction";
 import {Nesting} from "./ReactRouterDom/02_Nesting/02_nesting";
+import {UseParams} from "./ReactRouterDom/03_useParams/03_useParams";
+import {Link} from "./ReactRouterDom/04_Link/04_link";
 
 function App() {
     return (
         <div className="App">
             {/*<Introduction/>*/}
-            <Nesting/>
+            {/*<Nesting/>*/}
+            {/*<UseParams/>*/}
+            <Link/>
         </div>
     );
 }
